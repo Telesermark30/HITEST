68a4d6dea8c2efa1bc678ec4dd12055c08ac59ff
Telesermark30 <112105705+Telesermark30@users.noreply.github.com>
2022-10-20 20:03:23 +0800

    Update RobuxGenerator.java

--- RobuxGenerator/src/robuxgenerator/RobuxGenerator.java
+++ RobuxGenerator/src/robuxgenerator/RobuxGenerator.java
@@ -14,7 +14,7 @@ public class RobuxGenerator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("parkour kidz");
     }
     
 }

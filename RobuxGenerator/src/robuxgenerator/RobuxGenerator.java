ada9d891221f3865c40d323810dd37e11c3f6bf2
CapillanesFrizty <92376866+CapillanesFrizty@users.noreply.github.com>
2022-10-20 20:00:56 +0800

    Update RobuxGenerator.java

--- RobuxGenerator/src/robuxgenerator/RobuxGenerator.java
+++ RobuxGenerator/src/robuxgenerator/RobuxGenerator.java
@@ -14,8 +14,7 @@ public class RobuxGenerator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("10$Robux");
-        System.out.println("Its a Scam!!");
+        System.out.println("Chnages");
     }
     
 }
